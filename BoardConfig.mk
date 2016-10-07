381be9a7c3f949cd987cbb79961134e7d630a441
audahadi <wan.audahadi@gmail.com>
2016-10-07 22:13:18 +0800

    initial wifi bring up

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,6 +80,18 @@ AUDIO_FEATURE_DISABLED_HWDEP_CAL := true
 DOLBY_DDP := true
 DOLBY_DAP := true
 
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+#TARGET_USES_QCOM_WCNSS_QMI := true
+
 
 # Added to indicate that protobuf-c is supported in this build
 PROTOBUF_SUPPORTED := false
