05473b40acb1e4a4a71c54b2b703ced233b8a01e
audahadi <wan.audahadi@gmail.com>
2017-01-03 15:04:47 +0530

    rebrand to Lineage

--- lineage.mk
+++ lineage.mk
@@ -2,13 +2,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from device specific
 $(call inherit-product, device/asus/Z010D/full_Z010D.mk)
 
-PRODUCT_NAME := cm_Z010D
+PRODUCT_NAME := lineage_Z010D
 PRODUCT_DEVICE := Z010D
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max
